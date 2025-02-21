920f408b88df3e92da553fd34e402772325cd34c
TharHtet236401 <tharhteta3@gmail.com>
2025-02-22 01:59:43 +0700

    Add dynamic status update functionality for submissions

--- submissions/urls.py
+++ submissions/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.create_submission_view, name='create_submission'),
+    path('update-status/<int:submission_id>/', views.update_status, name='update_status'),
 ]
 
--- submissions/views.py
+++ submissions/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from .models import Submission
 from django.contrib import messages
 from .forms import SubmissionForm
@@ -50,3 +50,20 @@ def create_submission_view(request):
     else:
         form = SubmissionForm()
     return render(request, 'submissions/create-submission.html', {'form': form})
+
+def update_status(request, submission_id):
+    try:
+        submission = get_object_or_404(Submission, id=submission_id)
+        status_type = request.POST.get('status_type')
+        new_status = request.POST.get('status') == 'true'
+
+        if status_type == 'reviewed':
+            submission.is_reviewed = new_status
+        elif status_type == 'verified':
+            submission.is_verified = new_status
+        
+        submission.save()
+        return JsonResponse({'success': True})
+    except Exception as e:
+        return JsonResponse({'success': False, 'error': str(e)})
+
